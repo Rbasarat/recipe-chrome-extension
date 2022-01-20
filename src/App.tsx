de5a95db40087b55a81329d25e90814f6afd6f86
Rasjaad <rasjaad10@msn.com>
2022-01-20 21:46:28 +0100

    POC to get content

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { getCurrentTabUId } from './chrome/utils';
+import { ChromeMessage, Sender } from './types';
 
-function App() {
+export default () => {
+
+  const [url, setUrl] = useState<string|undefined>('');
+  const [responseFromContent, setResponseFromContent] = useState<string>('');
+
+
+  useEffect(() => {
+    const queryInfo = {active: true, lastFocusedWindow: true};
+
+    chrome.tabs && chrome.tabs.query(queryInfo, tabs => {
+        const url = tabs[0].url;
+        setUrl(url);
+    });
+}, []);
+
+const sendTestMessage = () => {
+  const message: ChromeMessage = {
+      from: Sender.React,
+      message: "Hello from React",
+  }
+  console.log("pressed");
+
+  getCurrentTabUId((id) => {
+      id && chrome.tabs.sendMessage(
+          id,
+          message,
+          (responseFromContentScript) => {
+              setResponseFromContent(responseFromContentScript);
+          });
+  });
+};
+
   return (
     <div className="App">
       <header className="App-header">
@@ -10,17 +43,16 @@ function App() {
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+                        <p>URL:</p>
+                <p>
+                    {url}
+                </p>
+                <button onClick={sendTestMessage}>SEND MESSAGE</button>
+                <p>Response from content:</p>
+                <p>
+                    {responseFromContent}
+                </p>
       </header>
     </div>
   );
 }
-
-export default App;
